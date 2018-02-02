3da0b37527290c69d8a8d5dd01999e56652957fa
Paul Massardo <paul.massardo@gmail.com>
2018-02-02 14:42:53 -0500

    Add project files.

---
+++ DemoScope/DemoScope.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDemoScope
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.tbOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(7, 104)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(56, 23)
+        Me.btnEnter.TabIndex = 1
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(66, 104)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(56, 23)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(125, 104)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(56, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'tbOutput
+        '
+        Me.tbOutput.Location = New System.Drawing.Point(7, 7)
+        Me.tbOutput.Multiline = True
+        Me.tbOutput.Name = "tbOutput"
+        Me.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbOutput.Size = New System.Drawing.Size(174, 91)
+        Me.tbOutput.TabIndex = 0
+        '
+        'frmDemoScope
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(187, 139)
+        Me.Controls.Add(Me.tbOutput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmDemoScope"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Demo Scope"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents tbOutput As TextBox
+End Class
---
+++ DemoScope/DemoScope.vb
@@ -0,0 +1,95 @@
+﻿
+Option Strict On
+''' <summary>
+''' Author:         Alfred Massardo
+''' Project Name:   DemoScope
+''' Date:           02-Feb-2018
+''' Description:    Demonstrate class scope of variableFormLevelScope 
+'''                 by maintaining the variable's state/value,
+'''                 as long as, the form is open.
+''' </summary>
+Public Class frmDemoScope
+
+    ' declare a variable that will live for
+    ' as long as the form/class will live
+    ' this will also be available to all
+    ' procedure (subs/functons) with in
+    ' the class
+    Private variableFormLevelScope As Double = 0 ' form/class level scope
+
+    ''' <summary>
+    ''' btnEnter_Click - fires when the enter button is clicked and calls
+    '''                 the Add function to add two numbers and return 
+    '''                 the sum and assign it to the form/class level
+    '''                 variable
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        Dim numberOne As Double = 2 ' procedure level scope
+        Dim numberTwo As Double = 5 ' procedure level scope
+
+        variableFormLevelScope += Add(numberOne, numberTwo) ' call the Add function to add the 2 numbers
+
+        ' concatenate the string in the textbox to it self and then 
+        ' concatenate the current value of the form/class level variable
+        ' concatenate the Visual Basic Cariage return Line feed
+        tbOutput.Text += variableFormLevelScope.ToString & vbCrLf
+
+    End Sub
+
+    ''' <summary>
+    ''' Add - Adds two numbers
+    '''     *** Function is a self contained area of code that contains one or more lines of code that can be called by another procedure and returns a value to the calling code. 
+    ''' </summary>
+    ''' <param name="numberOne">Double</param>
+    ''' <param name="numberTwo">Double</param>
+    ''' <returns>Double - the sum of the two arguments passed to the Method/Function</returns>
+    Private Function Add(numberOne As Double, numberTwo As Double) As Double
+
+        ' declare a return value variable
+        Dim returnValue As Double ' procedure level scope
+
+        ' add number one parameter to number two parameter
+        ' and assign it to return value
+        returnValue = numberOne + numberTwo
+
+        ' return the return value variable
+        Return returnValue
+
+    End Function
+
+
+    ''' <summary>
+    ''' The Click event of the reset button Is fired when the
+    ''' user clicks the reset button when they wish to clear the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        ' reset the output text box
+        tbOutput.ResetText()
+
+        ' reset the form/class level 
+        ' scoped variable(s)
+        variableFormLevelScope = 0
+
+    End Sub
+
+    ''' <summary>
+    ''' btnExit_Click - when click will call the form method to close the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        ' Exit Application
+        Me.Close()
+
+        ' Closing the form will remove the variableFormLevelScope from memory
+
+    End Sub
+
+End Class
